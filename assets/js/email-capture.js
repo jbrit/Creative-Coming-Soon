67444edc10635b2338e5d917f6c76f7f618e6983
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-07-17 08:42:46 +0100

    Email  Capture Commit

--- assets/js/email-capture.js
+++ assets/js/email-capture.js
@@ -2,7 +2,7 @@ const emailField = document.getElementById("emailInput"),
   emailForm = document.getElementById("emailForm");
 emailForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  var email = emailField.innerText;
+  var email = emailField.value;
   try {
     const res = await axios.post(
       "https://hngmailapi.herokuapp.com/api/emails/",
@@ -13,19 +13,16 @@ emailForm.addEventListener("submit", async (e) => {
         headers: {
           "Content-Type": "application/json",
           Authorization: "Token 678a9821fb4363ea32d837376c29040041ea159b",
-          "Access-Control-Allow-Origin": "*",
         },
       }
     );
-    let message = await res.json();
-    console.log(message);
   } catch (error) {
     Swal.fire({
       icon: "error",
       title: "Oops...",
       text: "Your Email could not be saved",
     });
-    emailField.innerText = "";
+    emailField.value = "";
     return;
   }
   Swal.fire({
